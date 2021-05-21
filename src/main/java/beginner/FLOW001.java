ffd7add844b42724b8373f28f7ec68ef1cf63896
harsh sengar <harsh.sengar@ukg.com>
2021-05-21 23:49:47 +0530

    Day 3 problem

---
+++ src/main/java/beginner/FLOW001.java
@@ -0,0 +1,29 @@
+package beginner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FLOW001 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(reader.readLine());
+		
+		int[] arr = new int[n];
+		
+		int i=0;
+		
+		while(n-- > 0) {
+			String[] narr = reader.readLine().split("\\s");
+			arr[i] = Integer.parseInt(narr[0]) + Integer.parseInt(narr[1]);
+			i++;
+		}
+		
+		for(int a:arr)
+			System.out.printf("%d%n", a);
+	}
+
+}
