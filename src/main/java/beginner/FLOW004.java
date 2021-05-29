59e5de480db54c409467ee1fac52f24c99c3c2d7
harsh sengar <harsh.sengar@ukg.com>
2021-05-29 13:35:22 +0530

    Day 6 problem

---
+++ src/main/java/beginner/FLOW004.java
@@ -0,0 +1,22 @@
+package beginner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FLOW004 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		int itr = Integer.parseInt(reader.readLine());
+		
+		while(itr-- > 0) {
+			String number = reader.readLine();
+			
+			System.out.printf("%d%n", Integer.parseInt(String.valueOf(number.charAt(0)))+Integer.parseInt(String.valueOf(number.charAt(number.length()-1))));
+	
+		}	
+	}
+}
